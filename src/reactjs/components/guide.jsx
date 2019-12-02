af8bf758ff6e8eab0c00a8c0bde391454e8c646d
briancanspit <briancanspit@gmail.com>
2019-12-02 05:59:22 +0300

    Added svg with logic display on layout

---
+++ src/reactjs/components/guide.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Fade } from "react-reveal"
+import MelloSvg from "../img/mello.svg"
+
+const Guide = () => {
+  return (
+    <img
+      src={MelloSvg}
+      alt="User guide avatar"
+      style={{ width: "20%", height: "20%" }}
+    />
+  )
+}
+
+export default Guide
--- src/reactjs/components/layout.jsx
+++ src/reactjs/components/layout.jsx
@@ -1,11 +1,23 @@
 import React from "react"
+import { useSelector } from "react-redux"
 import { Fade } from "react-reveal"
 import NavBar from "./navbar"
+import Guide from "./guide"
+import "../styles/beforeSearch.css"
 
 const Layout = () => {
+  let submittedText = useSelector((state) => state.submitText),
+    searchedText = useSelector((state) => state.searchText)
   return (
     <Fade duration={1500} clear>
       <NavBar />
+      <div className="fillerDiv">
+        {searchedText.length > 0 && submittedText.length > 0 ? (
+          <h3>Show loader or feed</h3>
+        ) : (
+          <Guide />
+        )}
+      </div>
     </Fade>
   )
 }
