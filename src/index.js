943bb99bf34d5f47948395795e640760362e27da
briancanspit <briancanspit@gmail.com>
2019-12-02 03:51:48 +0300

    Renamed imports due to reducers dir mass rename

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { createStore } from "redux"
 import { Provider } from "react-redux"
-import rootReducer from "./reactjs/reducers"
+import rootReducer from "./reactjs/reducers/rootReducer"
 import App from "./reactjs/App"
 import "./index.css"
 
