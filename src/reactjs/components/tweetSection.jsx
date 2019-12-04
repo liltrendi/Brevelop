5b9ac58219f5d4a718d6d5ba241fede20c987694
briancanspit <briancanspit@gmail.com>
2019-12-04 17:31:09 +0300

    Enhanced tweet prototype; synced bg color with card

--- src/reactjs/components/tweetSection.jsx
+++ src/reactjs/components/tweetSection.jsx
@@ -1,46 +1,19 @@
 import React from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faRetweet, faHeart, faReply } from "@fortawesome/free-solid-svg-icons"
-import "../styles/tweetSection.css"
+import TweetStructure from "./tweetStructure"
 import FemaleAvatar from "../img/female.svg"
+import MaleAvatar from "../img/male.svg"
 
 const TweetSection = () => {
+  const female = {
+      src: FemaleAvatar
+    },
+    male = {
+      src: MaleAvatar
+    }
   return (
     <div>
-      <div className="card">
-        <div className="card-header tweetHeader">
-          <img className="avatar" src={FemaleAvatar} alt="Avatar" />
-          <div className="twitterNames">
-            <h6 className="displayName">Display Name</h6>
-            <p className="userName">@cool_username</p>
-          </div>
-        </div>
-        <div className="card-body">
-          <h5>
-            Tweet body here or long lorem ipsum text that can fit or maybe wrap
-            around this card to span multiple lines
-          </h5>
-          <div className="tweetFooter">
-            <p className="card-text">
-              <small className="text-muted">
-                13:47 <span>&#183;</span> 03 Dec 19
-              </small>
-            </p>
-            <div className="retweetDiv">
-              <FontAwesomeIcon icon={faRetweet} className="retweetIcon" />
-              <small className="retweetValue">32</small>
-            </div>
-            <div className="heartDiv">
-              <FontAwesomeIcon icon={faHeart} className="heartIcon" />
-              <small className="heartValue">7</small>
-            </div>
-            <div className="replyDiv">
-              <FontAwesomeIcon icon={faReply} className="replyIcon" />
-              <small className="replyValue">2</small>
-            </div>
-          </div>
-        </div>
-      </div>
+      <TweetStructure opts={female} />
+      <TweetStructure opts={male} />
     </div>
   )
 }
