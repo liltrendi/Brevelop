8c29cc1ed815a75e9b8798a162408cbba521b95b
briancanspit <briancanspit@gmail.com>
2019-12-05 05:44:38 +0300

    Adding rate check reduced cpu usage;tweets coming in smooth, for now

--- src/reactjs/components/getTweetStream.jsx
+++ src/reactjs/components/getTweetStream.jsx
@@ -6,6 +6,7 @@ const { remote } = window.require("electron")
 const TweetStream = (props) => {
   let dispatch = useDispatch(),
     keyword = useSelector((state) => state.submitText),
+    tweetSize = useSelector((state) => state.tweetCollection),
     twitStream = remote.getGlobal("GetTweets")(keyword)
 
   if (props.opts.streamCancelled) {
@@ -14,6 +15,10 @@ const TweetStream = (props) => {
   } else {
     twitStream.on("tweet", (tweet) => {
       dispatch(addTweetToState(tweet))
+      if (tweetSize.length > 30) {
+        twitStream.stop()
+        dispatch(addTweetToState(false))
+      }
     })
     twitStream.on("error", (error) => {
       twitStream.stop()
