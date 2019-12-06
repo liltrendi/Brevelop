6b00c7b5a9975a73dfaaaa691a4da1bb0106abf0
briancanspit <briancanspit@gmail.com>
2019-12-06 06:10:22 +0300

    Improved profile section

--- src/reactjs/components/profileStructure.jsx
+++ src/reactjs/components/profileStructure.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from "react"
 import { Fade } from "react-reveal"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faUser } from "@fortawesome/free-solid-svg-icons"
+import { faTwitter } from "@fortawesome/free-brands-svg-icons"
+import {
+  faUsers,
+  faUserPlus,
+  faPaperPlane
+} from "@fortawesome/free-solid-svg-icons"
 import MaleAvatar from "../img/male.svg"
 import "../styles/profileStructure.css"
 
@@ -40,9 +45,36 @@ class ProfileStructure extends Component {
       <Fade duration={600} top>
         <div className="card profileCard" style={this.state.fixate && styles}>
           <div className="card-header profileHeader">
-            <img src={MaleAvatar} className="avatar" alt="Avatar" />
+            <img src={MaleAvatar} className="profileAvatar" alt="Avatar" />
+            <div className="profileHeaderNames">
+              <h5>Brian Njogu</h5>
+              <h6>@briancanspit</h6>
+            </div>
           </div>
-          <div className="card-body profileBody">Body</div>
+          <div className="profileIcons">
+            <FontAwesomeIcon icon={faTwitter} className="profTwitterIcon" />
+            <FontAwesomeIcon icon={faUsers} className="profTwitterIcon" />
+            <FontAwesomeIcon icon={faUserPlus} className="profTwitterIcon" />
+          </div>
+          <div className="card-body profileBody">
+            User description and form to tweet
+            <div className="profileForm">
+              <form>
+                <div className="form-group">
+                  <textarea
+                    className="form-control"
+                    rows="10"
+                    value="Tweet something"
+                  ></textarea>
+                </div>
+                <div className="profileBtnContainer">
+                  <button className="btn btn-md btn-info profileTweetBtn">
+                    Tweet <FontAwesomeIcon icon={faPaperPlane} />
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </Fade>
     )
