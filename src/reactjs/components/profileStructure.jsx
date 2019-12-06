f13153cce77254a707377bb89ac29f756e052e22
briancanspit <briancanspit@gmail.com>
2019-12-06 05:26:41 +0300

    Fixated profile on scroll with class state

--- src/reactjs/components/profileStructure.jsx
+++ src/reactjs/components/profileStructure.jsx
@@ -1,41 +1,52 @@
-import React from "react"
+import React, { Component } from "react"
 import { Fade } from "react-reveal"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faUser } from "@fortawesome/free-solid-svg-icons"
 import MaleAvatar from "../img/male.svg"
 import "../styles/profileStructure.css"
 
-const ProfileStructure = props => {
-  return (
-    <Fade duration={600} top>
-      <div className="card profileCard">
-        <div className="card-header profileHeader">Top</div>
-        <div className="card-body profileBody">Body</div>
-      </div>
-    </Fade>
-  )
+class ProfileStructure extends Component {
+  constructor() {
+    super()
+    this.state = {
+      fixate: false
+    }
+  }
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll)
+  }
+
+  handleScroll = () => {
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop
+    if (winScroll >= 75) {
+      this.setState({ fixate: true })
+    } else {
+      this.setState({ fixate: false })
+    }
+  }
+
+  render() {
+    let styles = {
+      position: "fixed",
+      top: 20,
+      width: "35.35%"
+    }
+    return (
+      <Fade duration={600} top>
+        <div className="card profileCard" style={this.state.fixate && styles}>
+          <div className="card-header profileHeader">
+            <img src={MaleAvatar} className="avatar" alt="Avatar" />
+          </div>
+          <div className="card-body profileBody">Body</div>
+        </div>
+      </Fade>
+    )
+  }
 }
 
 export default ProfileStructure
-
-/*
- * 
- * .profileTopCard {
-	background: #e6e6e6 !important;
-	color: #ffc107 !important;	
-}
-
-.profileHeader {
-	border-bottom: none !important;
-}
- * 
- * 
-<div className="card profileTopCard">
-		<div className="card-header profileHeader">
-          <img className="avatar" src={MaleAvatar} alt="Avatar" />
-          <div className="profileStats">
-			<FontAwesomeIcon icon={faUser} className="retweetIcon" />
-          </div>
-        </div>
-      </div>
-      */
