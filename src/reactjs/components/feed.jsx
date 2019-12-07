03f9d0956d69f109ab5bf5be65fdf00eb061f298
briancanspit <briancanspit@gmail.com>
2019-12-07 03:09:56 +0300

    Improved conditional render logic in feed component

--- src/reactjs/components/feed.jsx
+++ src/reactjs/components/feed.jsx
@@ -5,30 +5,24 @@ import CallToAction from "./callToAction"
 import FeedLayout from "./feedLayout"
 
 const Feed = () => {
-  let showFeed = useSelector(state => state.showFeed),
-    searchText = useSelector(state => state.searchText),
-    submitText = useSelector(state => state.submitText)
+  let showFeed = useSelector(state => state.showFeed)
 
-  if (searchText.length > 0 && submitText.length === 0 && !showFeed) {
-    return <FeedLayout opts={false} />
+  if (showFeed) {
+    let streamCancelled = false
+    return (
+      <div>
+        <TweetStream opts={streamCancelled} />
+        <FeedLayout />
+      </div>
+    )
   } else {
-    if (showFeed) {
-      let streamCancelled = false
-      return (
-        <div>
-          <TweetStream opts={streamCancelled} />
-          <FeedLayout opts={true} />
-        </div>
-      )
-    } else {
-      let streamCancelled = true
-      return (
-        <div>
-          <TweetStream opts={streamCancelled} />
-          <CallToAction />
-        </div>
-      )
-    }
+    let streamCancelled = true
+    return (
+      <div>
+        <TweetStream opts={streamCancelled} />
+        <CallToAction />
+      </div>
+    )
   }
 }
 
